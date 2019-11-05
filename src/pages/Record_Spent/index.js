ee59dab10613f5beb6e2257fdd654c4cb5c759ae
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-11-05 14:56:10 -0300

    insert ref on each input to change field by clicking enter on virtual keyboard

--- src/pages/Record_Spent/index.js
+++ src/pages/Record_Spent/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { View } from 'react-native'
 
 import { Container, Input, Label, SubmitButton } from './styles'
@@ -8,6 +8,12 @@ export default function Record_Spent() {
   const [desription, setDescription] = useState('Lanche')
   const [valueSpent, setValueSpent] = useState('30,00')
   const [datePurchase, setDatePurchase] = useState('25/10/20')
+
+  const spendTypeField = useRef(null)
+  const valueSpentField = useRef(null)
+  const datePurchaseField = useRef(null)
+  const handleSubmitButton = useRef(null)
+
 
   const handleSubmit = () => {
     console.tron.log(expenseType)
@@ -23,36 +29,46 @@ export default function Record_Spent() {
         <Input
           placeholder={'Ex: Rolê'}
           onChangeText={text => setExpenseType(text)}
-          value={expenseType}/>
+          value={expenseType}
+          onSubmitEditing={() => spendTypeField.current.focus()}
+        />
       </View>
 
       <View style={{ marginTop: 20 }}>
         <Label>Decrição</Label>
         <Input
+          ref={spendTypeField}
           placeholder={'Ex: Lanche'}
           onChangeText={text => setDescription(text)}
-          value={desription}/>
+          value={desription} 
+          onSubmitEditing={() => valueSpentField.current.focus()}/>
       </View>
 
       <View style={{ marginTop: 20 }}>
         <Label>Valor</Label>
         <Input
+          ref={valueSpentField}
           placeholder={'Ex: 30.00'}
           onChangeText={text => setValueSpent(text)}
-          value={valueSpent}/>
+          value={valueSpent} 
+          onSubmitEditing={() => datePurchaseField.current.focus()}/>
       </View>
 
       <View style={{ marginTop: 20 }}>
         <Label>Data</Label>
         <Input
+          ref={datePurchaseField}
           placeholder={'Ex: 25/10/20'}
           onChangeText={text => setDatePurchase(text)}
-          value={datePurchase}/>
+          value={datePurchase}
+          onSubmitEditing={handleSubmit}/>
       </View>
 
       <SubmitButton
+        ref={handleSubmitButton}
         onPress={handleSubmit}
       />
+      <View style={{ flex: 1 }}></View>
     </Container>
   )
 }
--- src/pages/Record_Spent/styles.js
+++ src/pages/Record_Spent/styles.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View`
+export const Container = styled.ScrollView`
   padding: 0 20px;
+  flex: 1;
   display: flex;
 `
 
